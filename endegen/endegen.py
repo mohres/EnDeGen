c903e8af99d8f4b3230548768d9dbeca1f5d14f6
mohres <mohres20@gmail.com>
2023-07-04 23:20:53 +0300

    Process the text

--- endegen/endegen.py
+++ endegen/endegen.py
@@ -81,3 +81,102 @@ text = read_text_from_file(path_to_file)
 # Get the number of unique characters
 vocab = get_unique_characters(text)
 # print(f"{len(vocab)} unique characters")
+
+
+"""## Process the text
+
+### Vectorize the text
+
+Before training, we need to convert the strings to a numerical representation.
+
+Using `tf.keras.layers.StringLookup` layer can convert each character into a numeric ID. 
+It just needs the text to be split into tokens first.
+"""
+
+
+def tokenize_text(texts):
+    """Tokenizes the given texts into individual characters.
+
+    Args:
+        texts (list): A list of strings to be tokenized.
+
+    Returns:
+        tf.RaggedTensor: A ragged tensor containing the tokens.
+    """
+    return tf.strings.unicode_split(texts, input_encoding="UTF-8")
+
+
+def create_string_lookup_layer(vocab):
+    """
+    Creates a StringLookup layer for converting tokens to character IDs.
+
+    Args:
+        vocab (list): A list of unique characters in the corpus.
+
+    Returns:
+        tf.keras.layers.StringLookup: The StringLookup layer.
+    """
+    return tf.keras.layers.StringLookup(vocabulary=vocab, mask_token=None)
+
+
+def create_inverse_string_lookup_layer(ids_from_chars):
+    """
+    Creates an inverse StringLookup layer for converting character IDs to tokens.
+
+    Args:
+        ids_from_chars (tf.keras.layers.StringLookup): The StringLookup layer used for token to ID conversion.
+
+    Returns:
+        tf.keras.layers.StringLookup: The inverse StringLookup layer.
+    """
+    chars_from_ids = tf.keras.layers.StringLookup(
+        vocabulary=ids_from_chars.get_vocabulary(), invert=True, mask_token=None
+    )
+    return chars_from_ids
+
+
+def convert_text_to_ids(ids_from_chars, chars):
+    """Converts tokens to character IDs using the StringLookup layer.
+
+    Args:
+        ids_from_chars (tf.keras.layers.StringLookup): The StringLookup layer for token to ID conversion.
+        chars (tf.RaggedTensor): The tokens to be converted to character IDs.
+
+    Returns:
+        tf.RaggedTensor: A ragged tensor containing the character IDs.
+    """
+    return ids_from_chars(chars)
+
+
+def convert_ids_to_text(chars_from_ids_, ids_):
+    """
+    Converts character IDs to tokens using the inverse StringLookup layer.
+
+    Args:
+        chars_from_ids_ (tf.keras.layers.StringLookup): The inverse StringLookup layer for ID to token conversion.
+        ids_ (tf.RaggedTensor): The character IDs to be converted to tokens.
+
+    Returns:
+        tf.Tensor: A tensor containing the recovered human-readable strings.
+    """
+    chars = chars_from_ids_(ids_)
+    text_ = tf.strings.reduce_join(chars, axis=-1)
+    return text_.numpy()
+
+
+ids_from_chars = create_string_lookup_layer(vocab)
+chars_from_ids = create_inverse_string_lookup_layer(ids_from_chars)
+
+
+def text_from_ids(ids_):
+    """
+    Recovers the human-readable strings from character IDs.
+
+    Args:
+        ids_ (tf.RaggedTensor): The character IDs to be converted to human-readable strings.
+
+    Returns:
+        str: The recovered human-readable strings.
+    """
+    return tf.strings.reduce_join(chars_from_ids(ids_), axis=-1).numpy()
+
